d5b105a806bc9d846d65f8bd1b28ff9b9de46511
Deva88 <devendra.raj.sdm@gmail.com>
2021-03-28 16:54:20 +0530

    Added: Create a contacts Address Book

--- src/com/addressbook/AddressBook.java
+++ src/com/addressbook/AddressBook.java
@@ -4,6 +4,7 @@ public class AddressBook {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Address Book");
+		
 
 	}
 
---
+++ src/com/addressbook/ContactInfo.java
@@ -0,0 +1,36 @@
+package com.addressbook;
+/*
+ * contact information has first name, last name, number,address
+ * state,city,zipcode, age and email
+ */
+
+public class ContactInfo {
+
+		String FIRST_NAME;
+		String LAST_NAME;
+		String ADDRESS;
+		String CITY;
+		String STATE;
+		String POSTAL_CODE;
+		String COUNTRY;
+		String CONTACT_NUMBER;
+		String EMAIL;
+		ContactInfo(String firstname,String lastname,String address,String city,String state,String zipcode,String country,String mob,String mail)
+		{
+			this.FIRST_NAME=firstname;
+			this.LAST_NAME=lastname;
+			this.ADDRESS=address;
+			this.CITY=city;
+			this.STATE=state;
+			this.POSTAL_CODE=zipcode;
+			this.COUNTRY=country;
+			this.CONTACT_NUMBER=mob;
+			this.EMAIL=mail;
+		}
+
+		public String toString() {
+			return String.format(FIRST_NAME+ " "+LAST_NAME+ " " +ADDRESS+ " "+CITY+ " "+STATE+" "+POSTAL_CODE+" "
+										 +COUNTRY+" "+CONTACT_NUMBER+" "+EMAIL);
+		}
+
+	}
