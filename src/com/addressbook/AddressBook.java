ed996383b341b749084daccee1bd632c061959c3
Deva88 <devendra.raj.sdm@gmail.com>
2021-04-04 06:44:03 +0530

    Added: Add multiple persons contact

--- src/com/addressbook/AddressBook.java
+++ src/com/addressbook/AddressBook.java
@@ -1,140 +1,56 @@
 package com.addressbook;
 import java.util.Scanner;
+import java.util.ArrayList;
 /*@Description- this class represents a address book.
  *contact information  */
 /*@Parameters- taken Contacts class object as variable so that we can
+ * Add multiple persons
  *  add a new contact to AddressBook*/
 public  class AddressBook {
-	Scanner sc=new Scanner(System.in);
-	static ContactInfo[] contactList=new ContactInfo[15];
-	int add(int increment)
-	{
-		System.out.println("Enter First Name:");
-		String fn=sc.next();
-		System.out.println("Enter last name:");
-		String ln=sc.next();
-		System.out.println("Enter Address:");
-		String add=sc.next();
-		System.out.println("Enter City:");
-		String city=sc.next();
-		System.out.println("Enter State:");
-		String state=sc.next();
-		System.out.println("Enter postal_code:");
-		String zip=sc.next();
-		System.out.println("Enter Country_name");
-		String country=sc.next();
-		System.out.println("Enter contact number:");
-		String mob=sc.next();
-		System.out.println("Enter email :");
-		String mail=sc.next();
-		contactList[increment++]=new  ContactInfo(fn, ln, add, city, state,zip,country,mob,mail);
-		return increment;
-	 }
-	void edit(int increment)
-	{
-		int flag;
-		for(flag=0;flag<10;flag++)
-		{
-		if(flag==increment)
-		{
-			System.out.println("Enter First Name:");
-			String fn=sc.next();
-      	System.out.println("Enter last name:");
-      	String ln=sc.next();
-      	System.out.println("Enter Address:");
-      	String add=sc.next();
-      	System.out.println("Enter City:");
-      	String city=sc.next();
-      	System.out.println("Enter State:");
-      	String state=sc.next();
-      	System.out.println("Enter postal_code:");
-      	String zip=sc.next();
-      	System.out.println("Enter Country:");
-      	String country=sc.next();
-      	System.out.println("Enter contact number:");
-      	String mob=sc.next();
-      	System.out.println("Enter email :");
-      	String mail=sc.next();
-      	contactList[increment++]=new  ContactInfo(fn, ln, add, city, state,zip,country,mob,mail);
-			break;
-		}
-		}
-		System.out.println("Your changes replicated to the Book manager successfully....");
-		
-	}
-	/*
-	 * deleting contact by name given by user and also checking whether address book
-	 * is empty or not
-	 */
-	void remove(int increment)
-	{
-		int flag;
-		for(flag=0;flag<15;flag++)
-			{
-				if(flag==increment)
-				{
-					contactList[increment]=null;
-					break;
-				}
-			}
-		System.out.println("!!Deleted!!");
-	}
-
-	void display()
-	{
-		for(int increment=0;increment<15;increment++)
-		{
-			System.out.println(increment+" "+contactList[increment]);
-		}
-	}
-	/*
-	 * using case in loop so that user can enter its choices to add, delete, edit or
-	 * print the contact in address book
-	 */
-public static void main(String args[]){
-	System.out.println("---------------WELCOME TO THE ADDRESS_BOOK_MANAGEMENT SYSTEM-------------"); 
-	Scanner sc=new Scanner(System.in);
-	AddressBook ab=new AddressBook();
-	int increment=0;
-	System.out.println("Enter valid option to perform Book-Manager_Application(1-->ENTER,2-->EXIT):");
-	int option=sc.nextInt();
-		while(option !=2) {
-			System.out.println("select the operations(1,2,3) you want to perform 1)Add 2)Edit 3)Remove 4)display \n");
-			int choose =sc.nextInt();
-			switch(choose) 
-			{
-				case 1:
-					System.out.println("Adding credentials to the Application");
-					increment=ab.add(increment);
-					break;
-				case 2:
-					System.out.println("welcome to the editing mode ..!");
-					System.out.println("Enter the row number(record) you want to edit");
-					int record_edit=sc.nextInt();
-					ab.edit(record_edit);
-					ab.display();
-					break;
-				case 3:
-					System.out.println("currently you are in Delete-mode ,once you delete it's not revert back \n");
-					System.out.println("Enter record number to delete :");
-					int record_del=sc.nextInt();
-					ab.remove(record_del);
-					break;
-				case 4:
-					System.out.println("-------These are the Records avalibale in your Application------- \n");
-					ab.display();
-					break;
-				default:
-					System.out.println("select valid operation[1/2/3/4]");
-			}
-		System.out.println("Enter choice(1-Enter/2-Exit):");
-		int option1=sc.nextInt();
-		if (option1==2)
-			{
-				System.out.println("OOPS!! you are choosing an Exit mode");
-				break;
-			}
-			}
-		System.out.println("THANKS FOR USING BOOK MANAGER-APPLICATION");
+	//Add multiple persons
+	static ArrayList listing = new ArrayList();
+	public static Scanner sc = new Scanner(System.in);
+	static AddressBook addressBook = new AddressBook();
+	static  final int i=0;
+	 public void addContact() {
+		System.out.println("Enter how many persons you want to add");
+		int numOfPerson=sc.nextInt();
+	    for(int i=0; i<=numOfPerson; i++)
+	    {
+	    	System.out.println("\nPerson :  "+i+" Enter First Name");
+	        String fn= sc.next();
+	        sc.nextLine();
+	        System.out.println("Person : " +i+ " Enter Last Name");
+	        String ln = sc.next();
+	        sc.nextLine();
+	        System.out.println("Person : " +i+ " Enter Address");
+	        String add = sc.next();
+	        sc.nextLine();
+	        System.out.println("Person : " +i+ " Enter City");
+	        String city = sc.nextLine();
+	        System.out.println("Person : " +i+ " Enter State");
+	        String state = sc.next();
+	        sc.nextLine();
+	        System.out.println("Person : " +i+ " Enter postal_Code");
+	        String zip = sc.next();
+	        System.out.println("Person : " +i+ " Enter Country_Name");
+	        String country = sc.next();
+	        System.out.println("person :" +i+ "Enter Mobile number");
+	        String mob=sc.next();
+	        System.out.println("Person : " +i+ " Enter E-mail");
+	        String mail = sc.next();
+	        ContactInfo contact = new ContactInfo(fn, ln, add, city, state, zip,country,mob, mail);
+	        listing.add(contact);
+	          
+	    }
+	   
+}
+	   
+public static void main(String args[]) {
+	
+	addressBook.addContact();	
+	System.out.println("--------The person records are listed below:--------\n");
+	System.out.println(listing);
+	System.out.println("\n contacts added sccessfully");
 	}
 }
--- src/com/addressbook/ContactInfo.java
+++ src/com/addressbook/ContactInfo.java
@@ -14,25 +14,22 @@ public class ContactInfo {
 	String COUNTRY;
 	String CONTACT_NUMBER;
 	String EMAIL;
-
-	ContactInfo(String firstname, String lastname, String address, String city, String state, String zipcode,
-			String country, String mob, String mail) {
-		this.FIRST_NAME = firstname;
-		this.LAST_NAME = lastname;
-		this.ADDRESS = address;
-		this.CITY = city;
-		this.STATE = state;
-		this.POSTAL_CODE = zipcode;
-		this.COUNTRY = country;
-		this.CONTACT_NUMBER = mob;
-		this.EMAIL = mail;
+	ContactInfo(String fn,String ln,String add,String city,String state,String zip,String country,String mob,String mail)
+	{
+		this.FIRST_NAME=fn;
+		this.LAST_NAME=ln;
+		this.ADDRESS=add;
+		this.CITY=city;
+		this.STATE=state;
+		this.POSTAL_CODE=zip;
+		this.COUNTRY=country;
+		this.CONTACT_NUMBER=mob;
+		this.EMAIL=mail;
 	}
 
-	// to print the contact details
-	@Override
 	public String toString() {
-		return String.format(FIRST_NAME + " " + LAST_NAME + " " + ADDRESS + " " + CITY + " " + STATE + " " + POSTAL_CODE
-				+ " " + COUNTRY + " " + CONTACT_NUMBER + " " + EMAIL);
+		return String.format(FIRST_NAME+" "+LAST_NAME+" "+ADDRESS+" "+CITY+" "+STATE+" "+POSTAL_CODE+" "
+									 +COUNTRY+" "+CONTACT_NUMBER+" "+EMAIL);
 	}
 
 }
